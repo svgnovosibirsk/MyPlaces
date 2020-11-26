2492d019c02be7c3e154a71906675e037c54e521
Sergey <a@a.com>
2020-11-26 16:12:12 +0700

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Sergey Golovin on 26.11.2020.
+//  Copyright © 2020 GoldenWindGames LLC. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var locationLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    
+
+}
