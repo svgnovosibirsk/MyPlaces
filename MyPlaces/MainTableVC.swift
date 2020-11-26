dab7996eaf2666b6cc3a08f3d2b18ef34fc81a4a
Sergey <a@a.com>
2020-11-26 14:39:41 +0700

    Lunch screen Row height

--- MyPlaces/MainTableVC.swift
+++ MyPlaces/MainTableVC.swift
@@ -28,8 +28,16 @@ class MainTableVC: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         
         return cell
+    }
+    
+    // MARK: - Table voew delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
 
     
