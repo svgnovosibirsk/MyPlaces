fe239c23c3b06de19ef5cebaa7598d755146bd9f
Sergey <a@a.com>
2020-12-01 14:42:04 +0700

    SearchBar make

--- MyPlaces/MainTableVC.swift
+++ MyPlaces/MainTableVC.swift
@@ -15,8 +15,19 @@ class MainTableVC: UIViewController, UITableViewDataSource, UITableViewDelegate 
     @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var reversedSortingBtn: UIBarButtonItem!
     
-    var places: Results<Place>!
-    var ascendingSorting = true
+    private var places: Results<Place>!
+    private var filteredPlaces: Results<Place>!
+    private var ascendingSorting = true
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    private var searchBarIsEmpty: Bool {
+        guard let text = searchController.searchBar.text else { return false }
+        return text.isEmpty
+    }
+    
+    private var isFiltering: Bool {
+        return searchController.isActive && !searchBarIsEmpty
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,18 +38,37 @@ class MainTableVC: UIViewController, UITableViewDataSource, UITableViewDelegate 
         segmentControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
         
         places = realm.objects(Place.self)
+        
+        // Setup search controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
 
     }
 
     // MARK: - Table view data source
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if isFiltering {
+            return filteredPlaces.count
+        }
+        
         return places.isEmpty ? 0 : places.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
-        let place = places[indexPath.row]
+        
+        var place = Place()
+        if isFiltering {
+            place = filteredPlaces[indexPath.row]
+        } else {
+            place = places[indexPath.row]
+        }
+        
         cell.nameLbl.text = place.name
         cell.locationLbl.text = place.location
         cell.typeLbl.text = place.type
@@ -79,7 +109,14 @@ class MainTableVC: UIViewController, UITableViewDataSource, UITableViewDelegate 
         
         if segue.identifier == "showDetail" {
             guard let indexPath = tableView.indexPathForSelectedRow else { return }
-            let place = places[indexPath.row]
+            
+            var place = Place()
+            if isFiltering {
+                place = filteredPlaces[indexPath.row]
+            } else {
+                place = places[indexPath.row]
+            }
+            
             let newPlaceVC = segue.destination as! NewPlaceTableVC
             newPlaceVC.currentPlace = place
         }
@@ -112,5 +149,19 @@ class MainTableVC: UIViewController, UITableViewDataSource, UITableViewDelegate 
         
         tableView.reloadData()
     }
+    
+}
+
+extension MainTableVC: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
+    private func filterContentForSearchText(_ searchText: String) {
+        filteredPlaces = places.filter("name CONTAINS[c] %@ OR location CONTAINS[c] %@", searchText, searchText)
+        tableView.reloadData()
+    }
+    
     
 }
